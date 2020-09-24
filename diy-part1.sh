e6a93a33f88e6cd53e01000b7b14deed923bc75c
adlerhow <32662188+tstart@users.noreply.github.com>
2020-09-25 00:02:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # add passwall
-sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall' feeds.conf.default
+#sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
 
